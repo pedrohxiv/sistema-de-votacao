949e6df4dd3843f6307b0a6cea6fb813b471f2ba
Pedro Henrique <pedrohalmeidamendonca@gmail.com>
2023-11-02 02:55:20 -0300

    Requisito 04

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,8 +1,32 @@
 package com.betrybe.sistemadevotacao;
 
+import java.util.ArrayList;
+
 /**
  * Classe para o gerenciamento de votação.
  */
-public class GerenciamentoVotacao {
+public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
+  /**
+   * Atributos.
+   */
+  private ArrayList<PessoaCandidata> pessoasCandidatas = new ArrayList<>();
+  private ArrayList<PessoaEleitora> pessoasEleitoras = new ArrayList<>();
+  private ArrayList<String> cpfsComputados = new ArrayList<>();
+
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+
+  }
+
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+
+  }
+
+  public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+
+  }
+
+  public void mostrarResultado() {
+
+  }
 }
